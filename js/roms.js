b2694825002651262b4d69feb48adc3c5e5fd73f
Jaqline <jaompedroreis@gmail.com>
2026-03-26 05:11:15 -0300

    Update v0.3 Beta
    26/03/2026
    - Novos jogos adicionado
    - Sistema de filtros adicionado
    - Changelog adicionado

--- js/roms.js
+++ js/roms.js
@@ -1,10 +1,16 @@
 const ROMS = {
 
  mario1: "https://dl.dropboxusercontent.com/scl/fi/l53t6mxg6oy6eef2cgupo/mario1.nes?rlkey=nga6ilmmngiitdo3qidwhk2s2&st=bnmwr7tm&dl=0",
+ mario2: "https://dl.dropboxusercontent.com/scl/fi/ycldsi8obvruq1hm6ub39/mario2.nes?rlkey=3yxmau6d87yuhq8dej76qsxqy&st=1nxb6ysn&dl=0",
+ mario3: "https://dl.dropboxusercontent.com/scl/fi/0dkve20lkehhrtlj906px/mario3.nes?rlkey=o0qy1zjzvnpodg7slal8exmvm&st=ga30rfti&dl=0",
+ marioworld: "https://dl.dropboxusercontent.com/scl/fi/izv9xi9jiqnq74iix7zp0/marioworld.sfc?rlkey=uoaw5ty9ga6jgqpn3cqv7az9f&st=uz7u4z2a&dl=0",
+ mario64: "https://dl.dropboxusercontent.com/scl/fi/did9wu9yqgtg0azcekdhr/mario64.z64?rlkey=mlsz6arxna5z3drf07d7voipd&st=bqsoj5s3&dl=0",
  pokemonred: "https://dl.dropboxusercontent.com/scl/fi/cjz0efq19yn5py9jngaot/pokemonred.gb?rlkey=tseyxucfjq5tbzi06e7ynuh6q&st=rs5xpvvm&dl=0",
  kirbydc: "https://dl.dropboxusercontent.com/scl/fi/ruwqup0jocjapnj8nk6lz/kirbydc.sfc?rlkey=mryh7dzgdm5icro8uu6fpf5tf&st=3vahcxhn&dl=0",
+ kirbynidl: "https://dl.dropboxusercontent.com/scl/fi/vfqaf18zp66waqvr2a93w/knidl.gba?rlkey=yxhhy3ejmj8j6u4hyma5f8sqp&st=ezcucz9i&dl=0",
  dkcountry: "https://dl.dropboxusercontent.com/scl/fi/61lkkqk0jflhsvcjm9eh5/dkcountry.sfc?rlkey=b4ttr4p1ccmvb7orv1biu1nhi&st=aa4ox2zf&dl=0",
- mario64: "https://dl.dropboxusercontent.com/scl/fi/did9wu9yqgtg0azcekdhr/mario64.z64?rlkey=mlsz6arxna5z3drf07d7voipd&st=bqsoj5s3&dl=0",
- tntetris: "https://dl.dropboxusercontent.com/scl/fi/8uny7indmtyozyerdxhyt/tetris64.n64?rlkey=863wg6zgbx9fsn4rggr4sqczo&st=d8uogne8&dl=0"
-
+ tntetris: "https://dl.dropboxusercontent.com/scl/fi/8uny7indmtyozyerdxhyt/tetris64.n64?rlkey=863wg6zgbx9fsn4rggr4sqczo&st=d8uogne8&dl=0",
+ sbomber2: "https://dl.dropboxusercontent.com/scl/fi/9j24s2imvcc12psv3w590/sbomber2.sfc?rlkey=bov2cs6nrj6jxvdl6tfva2cqb&st=ex6a5rf1&dl=0",
+ sbomber4: "https://dl.dropboxusercontent.com/scl/fi/3l89s2iy2l0d8g7zxvbs3/sbomber4.sfc?rlkey=wq9s5guxxrh8y69dzwfij5k9d&st=jer0ek20&dl=0",
+ sbomber5: "https://dl.dropboxusercontent.com/scl/fi/16my9z8cu10vvp27nrgzh/sbomber5.sfc?rlkey=03rdrioi8h8f75ugo6wu5ngp7&st=ev0vlrfi&dl=0"
 };
--- js/script.js
+++ js/script.js
@@ -13,11 +13,18 @@ function createGame(title, core, romKey){
 
 const games = [
  createGame("Super Mario Bros", "nes", "mario1"),
+ createGame("Super Mario Bros 2", "nes", "mario2"),
+ createGame("Super Mario Bros 3", "nes", "mario3"),
+ createGame("Super Mario World", "snes", "marioworld"),
+ createGame("Mario 64", "n64", "mario64"),
  createGame("Pokemon Red", "gb", "pokemonred"),
  createGame("Kirby's Dream Course", "snes", "kirbydc"),
+ createGame("Kirby: Nightmare in Dream Land", "gba", "kirbynidl"),
  createGame("Donkey Kong Country", "snes", "dkcountry"),
- createGame("Mario 64", "n64", "mario64"),
- createGame("The New Tetris", "n64", "tntetris")
+ createGame("The New Tetris", "n64", "tntetris"),
+ createGame("Super Bomberman 2", "snes", "sbomber2"),
+ createGame("Super Bomberman 4", "snes", "sbomber4"),
+ createGame("Super Bomberman 5", "snes", "sbomber5")
 ];
 
 // FILTER
@@ -80,3 +87,83 @@ window.addEventListener("load", () => {
 });
 
 renderGames();
+
+const changelog = [
+ {
+  version: "Beta v0.3",
+  date: "26/03/2026",
+  added: ["Super Mario Bros 2", "Super Mario Bros 3", "Super Mario World", "Super Bomberman 2", "Super Bomberman 4", "Super Bomberman 5", "Kirby Nightmare in Dream Land"],
+  changes: [
+   "Sistema de filtros adicionado",
+   "Changelogs adicionado"
+  ]
+ }
+];
+
+function renderChangelog(){
+
+ const container = document.getElementById("changelog");
+
+ changelog.forEach(log => {
+
+  const div = document.createElement("div");
+  div.className = "log";
+
+  div.innerHTML = `
+   <h3>${log.version}</h3>
+   <span class="date">${log.date}</span>
+
+   <p>🎮 Novos jogos:</p>
+   <ul>${log.added.map(g => `<li>${g}</li>`).join("")}</ul>
+
+   <p>⚙️ Mudanças:</p>
+   <ul>${log.changes.map(c => `<li>${c}</li>`).join("")}</ul>
+  `;
+
+  container.appendChild(div);
+
+ });
+
+}
+
+const changelogBtn = document.getElementById("changelog-btn");
+const changelogBox = document.getElementById("changelog");
+
+changelogBtn.onclick = () => {
+ changelogBox.classList.toggle("open");
+
+ if(changelogBox.classList.contains("open")){
+  document.getElementById("dim").style.opacity = "1";
+ } else {
+  document.getElementById("dim").style.opacity = "0";
+ }
+};
+
+// render no container certo
+function renderChangelog(){
+
+ const container = document.getElementById("changelog-content");
+ container.innerHTML = "";
+
+ changelog.forEach(log => {
+
+  const div = document.createElement("div");
+  div.className = "log";
+
+  div.innerHTML = `
+   <h3>${log.version}</h3>
+   <span class="date">${log.date}</span>
+
+   <p class="section-title">🎮 Novos jogos</p>
+   <ul>${log.added.map(g => `<li>${g}</li>`).join("")}</ul>
+
+   <p class="section-title">⚙️ Mudanças</p>
+   <ul>${log.changes.map(c => `<li>${c}</li>`).join("")}</ul>
+  `;
+
+  container.appendChild(div);
+
+ });
+}
+
+renderChangelog();
